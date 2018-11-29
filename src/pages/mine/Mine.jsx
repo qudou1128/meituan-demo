ca7cbcec96ac23456ab99f20bc74af6a6d4a2c63
qudou1128 <1695298586@qq.com>
2018-11-29 21:15:57 +0800

    登录
    git commit -m 登录

--- src/pages/mine/Mine.jsx
+++ src/pages/mine/Mine.jsx
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
-
+import './Mine.scss'
 
 class Mine extends Component {
     render(){
         return (
             <div id="Mine"  className="page">
-                <h1>这是Mine组件</h1>
+             {/* // nav头部 */}
+                <header className="nav_bar">
+                    <div className="nav_left">
+                        <a href="#"><img src="/return_icon.png"/></a>
+                    </div>
+                    <div className="nav_login">
+                        <h3>登录</h3>
+                    </div>
+                    <div className="nav_right">
+                        <a href="#" className="nav_regist">立即注册</a>
+                    </div>
+                </header>
+                {/* 登录页面*/}
+                <div className="login">
+                    <div className="login_list">
+                        <div className="login_active">
+                            <a href="#" className="react">手机验证登录</a>
+                        </div>
+                        <div className="login_normal">
+                            <a href="#" className="react">美团账号登录</a>
+                        </div>
+                    </div>
+                </div>
             </div>
+          
         )
 
     }
